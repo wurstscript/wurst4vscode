e3e6bfb4802c705f83025a7e84d6703172948416
Peter Zeller <Peter.peq@googlemail.com>
2016-04-24 17:17:54 +0200

    hover provider

--- src/features/hoverProvider.ts
+++ src/features/hoverProvider.ts
@@ -1,7 +1,7 @@
 'use strict';
 
 import AbstractProvider from './abstractProvider';
-import {HoverProvider, Hover, TextDocument, CancellationToken, Position} from 'vscode';
+import {HoverProvider, Hover, MarkedString, TextDocument, CancellationToken, Position} from 'vscode';
 
 export default class WurstHoverProvider extends AbstractProvider implements HoverProvider {
 
@@ -15,9 +15,47 @@ export default class WurstHoverProvider extends AbstractProvider implements Hove
         });
         return fut.then(value => {
             if (value) {
-                let contents = [value.documentation]
-                return new Hover(contents); 
+                let doc: string = value.documentation;
+                console.log(`description = ${doc}`);
+                return new Hover(this.fromHtml(doc)); 
             }
         });
 	}
+    
+    private fromHtml(html: string): MarkedString[] {
+        html = html.replace(/<pre>/g, "\n[code]");
+        html = html.replace(/<\/pre>/g, "\n");
+        html = html.replace(/<br \/>/g, "\n");
+        let txt = this.plain(html)
+        return txt.split(/\n/).map(line => {
+            if (line.startsWith("[code]")) {
+                return { language: 'wurst', value: line.replace("[code]", "") };
+            }
+            return line;
+        });
+    }
+    
+    private _regExp = /<(\S*?).*?>((.|\r|\n)*?)<\/\1>/;
+
+    /**
+     * remove xml-tags from string
+     */
+    private plain(doc: string): string {
+
+        if (!doc) {
+            return doc;
+        }
+
+        let newDoc: string;
+
+        while (true) {
+            newDoc = doc.replace(this._regExp,(m, g1, g2, g3) => g2);
+            if (newDoc === doc) {
+                break;
+            }
+            doc = newDoc;
+        }
+        newDoc = newDoc.replace(/<(\S*?).*\/>/g,"");
+        return newDoc;
+    }
 }

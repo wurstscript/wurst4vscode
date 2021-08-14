24100728194c1abdb7a54162984539c75165a931
Frotty <Frotty@users.noreply.github.com>
2021-08-14 22:27:01 +0200

    prettier format

--- src/extension.ts
+++ src/extension.ts
@@ -5,29 +5,29 @@ import { workspace, ExtensionContext, LanguageConfiguration } from 'vscode';
 import { LanguageClient, LanguageClientOptions, ServerOptions, Executable } from 'vscode-languageclient';
 import * as fs from 'fs';
 
-import {registerCommands} from './features/commands'
-import {registerFileCreation} from './features/fileCreation'
+import { registerCommands } from './features/commands';
+import { registerFileCreation } from './features/fileCreation';
 
 export async function activate(context: ExtensionContext) {
-    console.log("Wurst extension activated!!")
+    console.log('Wurst extension activated!!');
 
     let config: LanguageConfiguration = {
         comments: {
-            lineComment: "//",
-            blockComment: ["/*","*/"]
+            lineComment: '//',
+            blockComment: ['/*', '*/'],
         },
         brackets: [
-            ["{", "}"],
-            ["[", "]"],
-            ["(", ")"]
+            ['{', '}'],
+            ['[', ']'],
+            ['(', ')'],
         ],
         indentationRules: {
             increaseIndentPattern: /^\s*(if|while|for|function|class|module|interface)\s.*$/,
             decreaseIndentPattern: /^\s*(else|end)\s.*$/,
-        }
+        },
     };
 
-	setupDecorators(context);
+    setupDecorators(context);
 
     vscode.languages.setLanguageConfiguration('wurst', config);
 
@@ -35,80 +35,90 @@ export async function activate(context: ExtensionContext) {
         (value) => console.log(`init done : ${value}`),
         (err) => console.log(`init error: ${err}`)
     );
-
 }
 
 function setupDecorators(context: ExtensionContext) {
-	let timeout: NodeJS.Timer | undefined = undefined;
+    let timeout: NodeJS.Timer | undefined = undefined;
     const extension = vscode.extensions.getExtension('peterzeller.wurst');
-	const path = extension!.extensionPath;
-	const compiletimeDecorator = vscode.window.createTextEditorDecorationType({
-		gutterIconPath: `${path}/images/gears.svg`,
-    	gutterIconSize: 'contain',
-	});
+    const path = extension!.extensionPath;
+    const compiletimeDecorator = vscode.window.createTextEditorDecorationType({
+        gutterIconPath: `${path}/images/gears.svg`,
+        gutterIconSize: 'contain',
+    });
 
-	let activeEditor = vscode.window.activeTextEditor;
+    let activeEditor = vscode.window.activeTextEditor;
 
-	function updateDecorations() {
-		if (!activeEditor) {
-			return;
-		}
-    	timeout = undefined;
-		const regEx = /@compiletime\s+(\s*(static|public|private)\s)*function.+/g;
-		const text = activeEditor.document.getText();
-		const compiletime: vscode.DecorationOptions[] = [];
-		let match;
-		while (match = regEx.exec(text)) {
-			const startPos = activeEditor.document.positionAt(match.index);
-			const endPos = activeEditor.document.positionAt(match.index + match[0].length);
-			const decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: 'This function will be executed at compiletime.' };
-			compiletime.push(decoration);
-		}
-		activeEditor.setDecorations(compiletimeDecorator, compiletime);
-	}
+    function updateDecorations() {
+        if (!activeEditor) {
+            return;
+        }
+        timeout = undefined;
+        const regEx = /@compiletime\s+(\s*(static|public|private)\s)*function.+/g;
+        const text = activeEditor.document.getText();
+        const compiletime: vscode.DecorationOptions[] = [];
+        let match;
+        while ((match = regEx.exec(text))) {
+            const startPos = activeEditor.document.positionAt(match.index);
+            const endPos = activeEditor.document.positionAt(match.index + match[0].length);
+            const decoration = {
+                range: new vscode.Range(startPos, endPos),
+                hoverMessage: 'This function will be executed at compiletime.',
+            };
+            compiletime.push(decoration);
+        }
+        activeEditor.setDecorations(compiletimeDecorator, compiletime);
+    }
 
-	function triggerUpdateDecorations() {
-		if (timeout) {
-			clearTimeout(timeout);
-		}
-		timeout = setTimeout(updateDecorations, 500);
-	}
+    function triggerUpdateDecorations() {
+        if (timeout) {
+            clearTimeout(timeout);
+        }
+        timeout = setTimeout(updateDecorations, 500);
+    }
 
-	if (activeEditor) {
-		triggerUpdateDecorations();
-	}
+    if (activeEditor) {
+        triggerUpdateDecorations();
+    }
 
-	vscode.window.onDidChangeActiveTextEditor(editor => {
-		activeEditor = editor;
-		if (editor) {
-			triggerUpdateDecorations();
-		}
-	}, null, context.subscriptions);
+    vscode.window.onDidChangeActiveTextEditor(
+        (editor) => {
+            activeEditor = editor;
+            if (editor) {
+                triggerUpdateDecorations();
+            }
+        },
+        null,
+        context.subscriptions
+    );
 
-	vscode.workspace.onDidChangeTextDocument(event => {
-		if (activeEditor && event.document === activeEditor.document) {
-			triggerUpdateDecorations();
-		}
-	}, null, context.subscriptions);
+    vscode.workspace.onDidChangeTextDocument(
+        (event) => {
+            if (activeEditor && event.document === activeEditor.document) {
+                triggerUpdateDecorations();
+            }
+        },
+        null,
+        context.subscriptions
+    );
 }
 
 async function startLanguageClient(context: ExtensionContext) {
     let clientOptions: LanguageClientOptions = {
-		// Register the server for Wurst-documents
-		documentSelector: ['wurst'],
-		synchronize: {
-			// Synchronize the setting section 'wurst' to the server
-			configurationSection: 'wurst',
+        // Register the server for Wurst-documents
+        documentSelector: ['wurst'],
+        synchronize: {
+            // Synchronize the setting section 'wurst' to the server
+            configurationSection: 'wurst',
             // Notify the server about file changes to '.wurst files contain in the workspace
             // currently disabled, because not working
             // using manual workaround in registerFileChanges instead
-			// fileEvents: workspace.createFileSystemWatcher('**/*.wurst')
-		}
-    }
+            // fileEvents: workspace.createFileSystemWatcher('**/*.wurst')
+        },
+    };
 
     let serverOptions = await getServerOptions();
 
-    let client = new LanguageClient("wurstLanguageServer", serverOptions, clientOptions);
+    let client = new LanguageClient('wurstLanguageServer', serverOptions, clientOptions);
     context.subscriptions.push(client.start());
 
     context.subscriptions.push(registerCommands(client));
@@ -124,50 +134,53 @@ function registerFileChanges(client: LanguageClient): vscode.FileSystemWatcher {
             changes: [
                 {
                     uri: uri.toString(),
-                    type: type
-                }
-            ]
+                    type: type,
+                },
+            ],
         };
-        client.sendNotification("workspace/didChangeWatchedFiles", args);
+        client.sendNotification('workspace/didChangeWatchedFiles', args);
     }
 
-    watcher.onDidCreate(uri => notifyFileChange(1, uri));
-    watcher.onDidChange(uri => notifyFileChange(2, uri));
-    watcher.onDidDelete(uri => notifyFileChange(3, uri));
+    watcher.onDidCreate((uri) => notifyFileChange(1, uri));
+    watcher.onDidChange((uri) => notifyFileChange(2, uri));
+    watcher.onDidDelete((uri) => notifyFileChange(3, uri));
     return watcher;
 }
 
 async function getServerOptions(): Promise<ServerOptions> {
-    let config = vscode.workspace.getConfiguration("wurst")
+    let config = vscode.workspace.getConfiguration('wurst');
 
     // TODO make configurable
-    let java = config.get<string>("javaExecutable") ?? 'java';
-    let javaOpts = config.get<string[]>("javaOpts") ?? [];
-    let wurstJar = (config.get<string>("wurstJar")?? '$HOME/.wurst/wurstscript.jar').replace("$HOME", require('os').homedir());
-    let debugMode = config.get<boolean>("debugMode")
+    let java = config.get<string>('javaExecutable') ?? 'java';
+    let javaOpts = config.get<string[]>('javaOpts') ?? [];
+    let wurstJar = (config.get<string>('wurstJar') ?? '$HOME/.wurst/wurstscript.jar').replace(
+        '$HOME',
+        require('os').homedir()
+    );
+    let debugMode = config.get<boolean>('debugMode');
 
     if (!(await doesFileExist(wurstJar))) {
-        let msg = `Could not find ${wurstJar}. Please configure 'wurst.wurstJar' in your settings.json`
+        let msg = `Could not find ${wurstJar}. Please configure 'wurst.wurstJar' in your settings.json`;
         vscode.window.showErrorMessage(msg);
         return Promise.reject(msg);
     }
 
-    let args = javaOpts.concat(["-jar", wurstJar, "-languageServer"])
+    let args = javaOpts.concat(['-jar', wurstJar, '-languageServer']);
     if (debugMode == true) {
         if (await isPortOpen(5005)) {
-            args = ["-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005,quiet=y"].concat(args);
+            args = ['-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005,quiet=y'].concat(args);
         }
     }
 
     let exec: Executable = {
         command: java,
-        args: args
+        args: args,
     };
 
     let serverOptions: ServerOptions = {
-		run : exec,
-		debug: exec
-	}
+        run: exec,
+        debug: exec,
+    };
     return serverOptions;
 }
 
@@ -183,15 +196,14 @@ function isPortOpen(port: number): Promise<boolean> {
     return new Promise((resolve, _reject) => {
         let net = require('net');
         let tester = net.createServer();
-        tester.once('error', function (err: { code: string; }) {
+        tester.once('error', function (err: { code: string }) {
             if (err.code == 'EADDRINUSE') {
                 resolve(false);
             }
         });
-        tester.once('listening', function() {
-            tester.close()
+        tester.once('listening', function () {
+            tester.close();
             resolve(true);
-
         });
         tester.listen(port);
     });
--- src/features/fileCreation.ts
+++ src/features/fileCreation.ts
@@ -1,28 +1,26 @@
 'use strict';
 
-import {TextDocument} from 'vscode';
+import { TextDocument } from 'vscode';
 import { workspace, Disposable, WorkspaceEdit, Position } from 'vscode';
-import {basename, extname} from 'path';
+import { basename, extname } from 'path';
 
 function onDocumentOpen(td: TextDocument) {
-	let extension = extname(td.fileName);
-	if (extension != ".wurst" && extension != ".jurst") {
-		return;
-	}
-	if (td.lineCount > 1 || td.getText().length > 0) {
-		return;
-	}
+    let extension = extname(td.fileName);
+    if (extension != '.wurst' && extension != '.jurst') {
+        return;
+    }
+    if (td.lineCount > 1 || td.getText().length > 0) {
+        return;
+    }
 
-	let packageName = basename(td.fileName, extension);
-	let newText = `package ${packageName}\n\n`;
+    let packageName = basename(td.fileName, extension);
+    let newText = `package ${packageName}\n\n`;
 
-	let edit = new WorkspaceEdit()
-	edit.insert(td.uri, new Position(1,1), newText)
-	workspace.applyEdit(edit);
-
-
+    let edit = new WorkspaceEdit();
+    edit.insert(td.uri, new Position(1, 1), newText);
+    workspace.applyEdit(edit);
 }
 
 export function registerFileCreation(): Disposable {
-	return workspace.onDidOpenTextDocument(onDocumentOpen);
+    return workspace.onDidOpenTextDocument(onDocumentOpen);
 }
